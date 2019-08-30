305373c441f6131f4338382077a84e5cd0d2072c
qq <550662350@qq.com>
2019-08-30 13:35:17 +0800

    wao

---
+++ WahahaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wahaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WahahaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WahahaApplication.class, args);
+    }
+
+}
